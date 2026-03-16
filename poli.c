10ea34a05c642aaccb52c939ac8bf802ad9c4fdf
Giovanni <giovannibgcastro@hotmail.com>
2026-03-16 18:27:23 +0000

    Inicio do projeto - Create e Delete

--- poli.c
+++ poli.c
@@ -1,18 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 #include "poli.h"
 
 polinomio * poli_create(int grau){
     // TODO: Implemente aqui a solucao para operacao create
 
-    return NULL;
+    polinomio *p = malloc(sizeof(polinomio));
+    if(p == NULL) return NULL;
+
+    p -> grau = grau; // Aloca o grau
+    p -> termo = 0;   // Inicializa os termos como 0
+
+    p->coeficientes = malloc((grau + 1) * sizeof(int)); // Aloca dinamicamente o vetor p/ a quantidade de coeficientes
+    if(p -> coeficientes == NULL){
+        free(p);
+        free(p -> coeficientes);
+        return NULL;
+    }
+
+    for(int i = 0; i <= grau; ++i) {
+        p -> coeficientes[i] = 0;
+    }
+
+    return p;
 }
 
 void poli_destroy(polinomio **p){
     // TODO: Implemente aqui a solucao para operacao destroy
+    if(p == NULL || *p == NULL) return;
 
-
+    free((*p) -> coeficientes);
+    
+    free(*p);
+    *p = NULL;
+    
     return;
 }
 
